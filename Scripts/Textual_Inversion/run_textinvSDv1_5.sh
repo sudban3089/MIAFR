7c76859dc273b9bc4bf94a78c99549906f644436
sudban3089 <sb9084@nyu.edu>
2024-06-24 14:41:44 -0400

    Update run_textinvSDv1_5.sh

--- Scripts/Textual_Inversion/run_textinvSDv1_5.sh
+++ Scripts/Textual_Inversion/run_textinvSDv1_5.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 export MODEL_NAME="runwayml/stable-diffusion-v1-5"
-export DATA_DIR="/scratch/sb9084/Dreambooth-Stable-Diffusion/IJCB/data/CelebA_TBIOM_rebuttal/20/TIDataset" #"./examples/20/SubID_0021_NumImages_20"
+export DATA_DIR="/path/to/images/for/each/subject/in/dataset" #"./examples/20/SubID_0021_NumImages_20"
 
-accelerate launch textual_inversion.py \
+accelerate launch textual_inversion_train.py \
   --pretrained_model_name_or_path=$MODEL_NAME \
   --train_data_dir=$DATA_DIR \
   --learnable_property="object" \
-  --placeholder_token="<ccorn-person>" \
+  --placeholder_token="<specialtoken-person>" \
   --initializer_token="person" \
   --resolution=512 \
   --train_batch_size=3 \
@@ -17,6 +17,6 @@ accelerate launch textual_inversion.py \
   --lr_scheduler="linear" \
   --lr_warmup_steps=500 \
   --num_vectors 5 \
-  --output_dir="./textualinvembs/textual_inversion_ccorn_softmargin_numvect5linear"
+  --output_dir="path/to/save/learnedembeds.bin"
 
 
